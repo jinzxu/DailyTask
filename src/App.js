8ae3241fac0d7da8bf1041d914bebfe08d9f82a9
Jason <jinzxu@ucalgary.ca>
2022-12-26 19:07:43 -0700

    Add useEffect for initial tasks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Component/Card";
 import TaskForm from "./Component/NewTask/TaskForm";
 import Tasks from "./Component/Task/Tasks";
+const INITIAL_TASKS = [
+  {
+    date: new Date(2021, 1, 1),
+    title: "running",
+    status: "todo",
+    id: 1
+  },
+  {
+    date: new Date(2021, 2, 1),
+    title: "eating",
+    status: "todo",
+    id: 2
+  },
+  {
+    date: new Date(2021, 2, 1),
+    title: "swimming",
+    status: "todo",
+    id: 3
+  }
+]
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      date: new Date(2021, 1, 1),
-      title: "running",
-      status: "todo",
-      id: 1
-    },
-    {
-      date: new Date(2021, 2, 1),
-      title: "eating",
-      status: "todo",
-      id: 2
-    },
-    {
-      date: new Date(2021, 2, 1),
-      title: "swimming",
-      status: "todo",
-      id: 3
-    }
-  ])
+  const [tasks, setTasks] = useState(INITIAL_TASKS)
+  useEffect(() => { setTasks(INITIAL_TASKS) }, [INITIAL_TASKS])
   const submitNewTaskHandler = (task) => {
     setTasks(prevTasks => (
       [
@@ -33,7 +35,7 @@ function App() {
   }
   const deleteTaskHandler = (deletedTask) => {
     setTasks(prevTasks => (
-      prevTasks.filter(task => task.id != deletedTask.id).map(task => task)
+      prevTasks.filter(task => task.id !== deletedTask.id).map(task => task)
     ))
   }
 
